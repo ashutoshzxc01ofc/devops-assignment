c62fb612477ea7371924814b4b865de06b3bc5e0
ashutoshverma02 <ashutosh.verma02@nagarro.com>
2023-11-03 11:53:12 +0530

    'firstcommit'

---
+++ src/test/java/com/nagarro/BenchAwsAssignment/BenchAwsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.BenchAwsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenchAwsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
